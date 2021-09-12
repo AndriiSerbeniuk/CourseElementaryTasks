fac5e0ab5347ca56bd812bf7dc641aeaa5c0377d
AndriiSerbeniuk <andriiserbeniuk@gmail.com>
2021-09-12 18:56:13 +0300

    Changed the way arguments are provided

--- TaskChessboard/src/main.cpp
+++ TaskChessboard/src/main.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 #include "string_drawer.h"
 
-int main() {
-	ChessBoard board(8, 8);		// Create a blank board
+int main(int argc, char* argv[]) {
+	if (argc != 3){
+    std::cout << "Wrong number of arguments provided.\n"
+      << "Pass two integers that correspond to board's width and height.\n";
+    return 0;
+  }
+  int w, h;
+  try {
+      w = std::stoi(argv[1]);
+    }
+  catch (const std::exception& e) {
+    std::cout << argv[1] << " is not an integer.\n";
+  }
+  try {
+      h = std::stoi(argv[2]);
+    }
+  catch (const std::exception& e) {
+    std::cout << argv[2] << " is not an integer.\n";
+    return 0;
+  }
+  if (w <= 0 || h <= 0) {
+    std::cout << w << "x" << h << " is not a valid size for a chess board."
+    << "\nEach dimention must be > 0.\n";
+    return 0;
+  }
+	ChessBoard board(w, h);		// Create a blank board
 	std::string drawn_board;	// String the board will be drawn on
-	
-	board.set_piece(1, 1, CHESS_PAWN, false);
-	board.set_piece(5, 4, CHESS_PAWN, true);
 	StringDrawer drawer(&board, drawn_board);
 	drawer.draw();
 	std::cout << drawn_board << "\n\n";
-
-	drawer.start_white(true);
-	drawer.draw();
-	std::cout << drawn_board;
 
 	return 0;
 }
